cff32531580e5be1aae31220bfe641646e5dac40
rhea-manuel <rheamanuel02@gmail.com>
2020-09-18 03:01:08 -0400

    Completed Task 6

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,5 +1,7 @@
 // const { values } = require("cypress/types/lodash")
 
+// const { values } = require("cypress/types/lodash")
+
 describe('Lambda Tests', () => {
 
     const button = () => cy.get('button')
@@ -8,25 +10,33 @@ describe('Lambda Tests', () => {
         cy.visit('http://localhost:3000/')
     })
 
+    
+
+    // Helper function for all input values
     function valueWorks(toCheck, content) {
         const item = () => cy.get(toCheck)
+
+        button().not('has.disabled')
 
         item()
             .type(content)
             .should('have.value', content)
     }
 
-    function checkedTest(){
+    // Function for the terms of service
+    function checkedTest() {
         cy.get('[name="terms"]')
-        .not('have.checked')
-        .click()
-        .should('have.checked')
+            .not('have.checked')
+            .click()
+            .should('have.checked')
     }
 
-    function rolesTest(toSelect){
+    // helper function for the roles
+    function rolesTest(toSelect) {
         cy.get('[name="role"]').select(toSelect)
     }
 
+    // Task 1: Name test
     it('Name test', () => {
         valueWorks('[name=name]', 'Rhea Manuel')
         // cy.get('[name=name]')
@@ -34,6 +44,7 @@ describe('Lambda Tests', () => {
         //     .should('have.value', 'Rhea Manuel')
     })
 
+    // Task 2: Email test
     it('Email test', () => {
         const email = () => cy.get('[name="email"]')
 
@@ -45,19 +56,26 @@ describe('Lambda Tests', () => {
 
         email().clear()
 
+        button()
+            .should('have.disabled')
+
         valueWorks('[name="email"]', 'working@domain.com')
     })
 
+    // Task 3 password test
     it('Password test', () => {
         valueWorks('[name="password"]', 'longpassword')
     })
 
+    // Task 4 terms of service test
     it('Terms of Service Test', () => {
         checkedTest()
 
     })
 
+    // Stretch goal (?) Roles test
     it('Roles test', () => {
+
         const allRoles = ['Frontend developer', 'Backend devloper', 'Designer']
 
         for (let i = 0; i < allRoles.length; i++) {
@@ -65,6 +83,7 @@ describe('Lambda Tests', () => {
         }
     })
 
+    // Task 5 submit test
     it('Can Submit', () => {
         valueWorks('[name=name]', 'Rhea Manuel')
         valueWorks('[name="email"]', 'working@domain.com')
@@ -73,9 +92,56 @@ describe('Lambda Tests', () => {
         rolesTest('Frontend developer')
 
         button()
-        .not('have.disabled')
-        .click()
-    
+            .not('have.disabled')
+            .click()
+
+    })
+
+    // TASK 6: Empty value. Kind of a long-winded way of doing it, but it works. 
+    // Basically: populates every field
+    it('Empty value', () => {
+
+        // Clicks terms
+        cy.get('[name="terms"]').click()
+
+        // Chooses a role
+        cy.get('[name="role"]').select('Frontend developer')
+
+        // Now, array for all the inputs
+        const name = { toCheck: '[name="name"]', content: 'Rhea Manuel' }
+        const email = { toCheck: '[name="email"]', content: 'working@domain.com' }
+        const password = { toCheck: '[name="password"]', content: 'longpassword' }
+
+        const toTest = [name, email, password]
+
+        // Array that contains elements to clear in the next round
+        let toClear = []
+
+        // Loops through all the inputs 
+        for (let i = 0; i < toTest.length; i++) {
+
+            // Clears the form for the next round
+            toClear.forEach((item)=>{
+                cy.get(item).clear()
+            })
+
+            // For every input that ISNT currently being checked for blank input, populates it with verifiably valid data
+            for (let other = 0; other < toTest.length; other++) {
+
+                if (other != i) {
+                    toClear.push(toTest[other].toCheck)
+                    valueWorks(toTest[other].toCheck, toTest[other].content)
+
+                }
+
+
+            }
+
+            // cy.log(toTest[i].toCheck)
+            // makes sure the button is disabled.
+            button().should('has.disabled')
+
+        }
     })
 
 })
